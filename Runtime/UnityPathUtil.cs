b8200a1f7597caafd40eda6a54be51edf2e332b9
Donozuka <saber-wing@e-mail.jp>
2021-09-29 11:15:47 +0900

    Change namespase

--- Runtime/UnityPathUtil.cs
+++ Runtime/UnityPathUtil.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Dono.UtilitiesForUnity
+namespace Dono.UtilitiesForUnity.Runtime
 {
     public static class UnityPathUtil
     {
