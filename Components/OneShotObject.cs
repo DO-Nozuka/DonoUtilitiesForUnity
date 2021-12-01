1398a4fa6b2b8d37c3225eedc866dcd7b0aa18df
Donozuka <saber-wing@e-mail.jp>
2021-12-01 13:51:01 +0900

    Add OneShotObject

---
+++ Components/OneShotObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Dono.UtilitiesForUnity.Components
+{
+    public class OneShotObject : MonoBehaviour
+    {
+        [SerializeField] private float lifeTime;
+
+        public void Start()
+        {
+            Destroy(gameObject, lifeTime);
+        }
+    }
+}
---
+++ Components/OneShotObjectWithDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Dono.UtilitiesForUnity.Components
+{
+    public class OneShotObjectWithDisplay : MonoBehaviour
+    {
+        [SerializeField] private float lifeTime;
+        [SerializeField] private float elapsedTime = 0;
+        private float _elapsedTime;
+
+        public void Update()
+        {
+            _elapsedTime += Time.deltaTime;
+            elapsedTime = _elapsedTime;
+            if (_elapsedTime > lifeTime)
+                Destroy(gameObject);
+        }
+    }
+}
